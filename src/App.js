c2a1d4fb9fd6de8a894e3d8878e48a8eb7841f0d
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-12-18 19:24:44 -0400

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Library from './components/library/library';
 
 class App extends Component {
   render() {
     return (
       <main role="main" className="App">
+        <Library />
       </main>
     );
   }
